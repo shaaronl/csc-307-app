e6172f6e2f38014390d52f83ed7247d5de6760ef
shaaronl <sharonliang617@gmail.com>
2024-04-05 19:52:33 -0700

    finished

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -4,13 +4,8 @@ import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor" // the rest of the data
-    }
-  ]);
-
+  const [characters, setCharacters] = useState([]);
+  
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
