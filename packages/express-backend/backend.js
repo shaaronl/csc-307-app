974cf88c5ca3ebb42ff0e40c010edb1c9caec77d
shaaronl <sharonliang617@gmail.com>
2024-04-09 19:31:00 -0700

    added nodemon + findUserByName

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,7 +45,19 @@ const users = {
     }
   ]
 };
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
